bfdec51c5887d1578acd87259252fe9f91ac3d68
zhuang kang <zhuang.kang@de.abb.com>
2025-01-07 20:49:24 +0100

    draw the figures

--- PPTFigure/plot.py
+++ PPTFigure/plot.py
@@ -2,15 +2,18 @@ import matplotlib.pyplot as plt
 
 # Data
 time_points = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-s1_values = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
-s2_values = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+s1_values_1 = [1, 1, 1, 0, 0, 0, 0, 0, 0, 1]
+s1_values_2 = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
 
+s2_values_1 = [1, 1, 1, 0, 0, 1, 1, 1, 0, 0]
+s2_values_2 = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
 # Create figure and subplots
-plt.figure(figsize=(10, 6))
+plt.figure(figsize=(10, 3))
 
 # First plot
 plt.subplot(2, 1, 1)  # 2 rows, 1 column, 1st subplot
-plt.plot(time_points, s1_values, marker='o', linestyle='-', color='b')
+plt.plot(time_points, s1_values_1, marker='o', linestyle='-', color='b')
+plt.plot(time_points, s1_values_2, marker='x', linestyle='-', color='g')
 plt.title("Good")
 plt.xlabel("Time")
 plt.ylabel("S1")
@@ -19,7 +22,8 @@ plt.grid(True)
 
 # Second plot
 plt.subplot(2, 1, 2)  # 2 rows, 1 column, 2nd subplot
-plt.plot(time_points, s2_values, marker='o', linestyle='-', color='r')
+plt.plot(time_points, s2_values_1, marker='o', linestyle='-', color='r')
+plt.plot(time_points, s2_values_2, marker='x', linestyle='-', color='y')
 plt.title("Bad")
 plt.xlabel("Time")
 plt.ylabel("S2")
